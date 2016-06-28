f73bde3597456fe781534b12b2ed3d32a133b6f9
CMYKpixels <julien.alsina@gmail.com>
2016-06-28 10:08:40 +0200

    Maj Cours

---
+++ Fichiers Récap/copyright/copyright_end.php
@@ -0,0 +1,13 @@
+<?php
+
+function copyright($startYear) {
+    $currentYear = date('Y');
+    if ($startYear < $currentYear) {
+        $currentYear = date('y');
+        return "&copy; $startYear&ndash;$currentYear";
+    } else {
+        return "&copy; $startYear";
+    }
+}
+
+echo copyright(2017);
